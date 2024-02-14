c4893aa174433cf7bdeb4d4cef9c4703c91de6fd
Corro-Kun <networpava@gmail.com>
2024-02-14 13:14:12 -0500

    add video-wallpaper

---
+++ config/video-wallpapers/changer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Cambiar el fondo de pantalla con el video
+~/.config/video-wallpapers/setwall
+
+# Notificar el cambio de fondo de pantalla usando Dunst
+dunstify "Wallpaper changed"
